b38a409704b7d252b4528d04bd879094a386f2a5
Fernando Serpa <serpaef@outlook.com>
2022-04-17 23:38:31 -0300

    fix: conserta problema de seleção de chamada
    onde o ultimo valor não era selecionado adequadamente

--- src/components/PriceCalculator.js
+++ src/components/PriceCalculator.js
@@ -4,6 +4,7 @@ import { Form, Row, Col, Container, FloatingLabel } from 'react-bootstrap';
 const CALC_RATE_DIFF = 0.1;
 
 function PriceCalculator({ rates, plans }) {
+  const [rateId, setRateId] = useState(0);
   const [ratePrice, setRatePrice] = useState(0);
   const [planMinutes, setPlanMinutes] = useState(0);
   const [callMinutes, setCallMinutes] = useState(0);
@@ -11,38 +12,42 @@ function PriceCalculator({ rates, plans }) {
   const [priceWithoutPlan, setPriceWithoutPlan] = useState(0);
 
   // better than repeat it every time on 'onChange'
-  const changeController = (setter, {target: {value}}) => {
+  const changeController = (setter, { target: { value } }) => {
     setter(+value);
+
+    const {pricePerMinute} = rates.find(rate => rate.id === rateId)
+    
+    setRatePrice(pricePerMinute)
     calcPriceWithPlan();
     calcPriceWithoutPlan();
-  }
+  };
 
   // it sets the price to 0 if call minutes <= plan minutes
   // otherwise, it calculates the final price with the excess minutes, with the 10% increase
   const calcPriceWithPlan = useCallback(() => {
     if (callMinutes <= planMinutes) {
-      setPriceWithPlan(() => 0)
+      setPriceWithPlan(() => 0);
     } else {
       const minutesToCalc = callMinutes - planMinutes;
-      const rate = ratePrice + (ratePrice * CALC_RATE_DIFF);
+      const rate = ratePrice + ratePrice * CALC_RATE_DIFF;
       const callPrice = minutesToCalc * rate;
       setPriceWithPlan(callPrice);
     }
-  }, [callMinutes, planMinutes, ratePrice])
+  }, [callMinutes, planMinutes, ratePrice]);
 
   // well, this one just makes the math.
   const calcPriceWithoutPlan = useCallback(() => {
     const callPrice = ratePrice * callMinutes;
     setPriceWithoutPlan(() => callPrice);
-  }, [callMinutes, ratePrice])
+  }, [callMinutes, ratePrice]);
 
   // setting up initial state on rendering with api data.
   useEffect(() => {
-    if(rates && plans) {
+    if (rates && plans) {
       setRatePrice(rates[0].pricePerMinute);
       setPlanMinutes(plans[0].minutes);
     }
-  },[plans, rates])
+  }, [plans, rates]);
 
   // updating total prices on component.
   useEffect(() => {
@@ -61,14 +66,14 @@ function PriceCalculator({ rates, plans }) {
               className='mb-2'
             >
               <Form.Select
-                value={ratePrice}
-                onChange={(event) => changeController(setRatePrice, event)}
+                value={rateId}
+                onChange={(event) => changeController(setRateId, event)}
               >
                 {rates &&
                   rates.map((rate) => (
                     <option
                       key={rate.id}
-                      value={rate.pricePerMinute}
+                      value={rate.id}
                     >{`De ${rate.origin} para ${rate.destination}`}</option>
                   ))}
               </Form.Select>
